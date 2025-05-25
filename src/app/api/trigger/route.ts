c22c7867ebbb0c9f05528b3efcc3c40ba8a426d6
Ph-Hao <phannhathao218@gmail.com>
2025-05-25 13:22:50 +0700

    Edit route to show request information on console

--- src/app/api/trigger/route.ts
+++ src/app/api/trigger/route.ts
@@ -1,7 +1,21 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export async function POST() {
+// export async function POST() {
+//     const message = 'API has been triggered successfully!';
+//     console.log(message);
+//     return NextResponse.json({ message });
+// }
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
     const message = 'API has been triggered successfully!';
+    const requestInfo = {
+        method: request.method,
+        url: request.url,
+        headers: Object.fromEntries(request.headers),
+    };
+    console.log('Request Info:', requestInfo);
     console.log(message);
     return NextResponse.json({ message });
 }
