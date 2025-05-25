2fad697d05e46e5beae68367a8bbb15b608c855c
Ph-Hao <phannhathao218@gmail.com>
2025-05-25 14:46:41 +0700

    Simply API triggered log information

--- src/app/api/trigger/route.ts
+++ src/app/api/trigger/route.ts
@@ -1,21 +1,7 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// export async function POST() {
-//     const message = 'API has been triggered successfully!';
-//     console.log(message);
-//     return NextResponse.json({ message });
-// }
-
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function POST(request: NextRequest) {
+export async function POST() {
     const message = 'API has been triggered successfully!';
-    const requestInfo = {
-        method: request.method,
-        url: request.url,
-        headers: Object.fromEntries(request.headers),
-    };
-    console.log('Request Info:', requestInfo);
     console.log(message);
     return NextResponse.json({ message });
 }
